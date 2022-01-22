ca0f7473848e50560db68306c640f006310e745f
9n8 <87565211+9n8@users.noreply.github.com>
2022-01-22 23:04:49 +0300

    Update afkcounter.py

--- afkcounter.py
+++ afkcounter.py
@@ -1,32 +1,29 @@
-import discord, random, os
+import discord, random, os, time
 from discord.ext import commands
- 
-input("Press Enter To Continue...")
-os.system('cls')
+
 token = input("Enter Token: ")
-client = commands.Bot(command_prefix=commands.when_mentioned_or("$"))
-os.system('cls')
-os.system(f'title [AFK COUNTER]')
-os.system(f'mode 60,20')
- 
 
-
- 
-@client.event
-async def on_ready():
-  print(f""" \u001b[31m
-╔═╗╔═╗╦╔═
-╠═╣╠╣ ╠╩╗
-╩ ╩╚  ╩ ╩ @9n8
-------------------------------
+class counter(discord.Client):
+    async def on_ready(self):
+        os.system('cls')
+        os.system(f'title [COUNTER]')
+        os.system(f'mode 30,15')
+        print(f""" \u001b[31m
+         ╔═╗╔═╗╦╔═
+         ╠═╣╠╣ ╠╩╗
+         ╩ ╩╚  ╩ ╩ \u001b[35m@9n8
+       \u001b[33m---------------
 """)
- 
-@client.event
-async def on_message(message):
-    channel = message.channel
-    if message.content.startswith('afk'):
-      for i in range(1, 101):
-        await message.channel.send(i)
+       
+        print('\u001b[32m    logged in as {0}'.format(self.user))
+    async def on_message(self, message):
+        channel = message.channel
+        if 'afk' in message.content:
+          for i in range(1, 1001):
+            await message.channel.send(i)
+      
 
- 
+client = counter()
+
+
 client.run(token, bot=False)
